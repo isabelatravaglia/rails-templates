4356271e607987b0bac768cc59f6c3a602666043
isabela santos <isabelatravaglia@gmail.com>
2021-03-09 14:47:10 +0000

    added padding

--- min_postgresql.rb
+++ min_postgresql.rb
@@ -137,8 +137,8 @@ end
 inject_into_file 'config/database.yml', after: 'encoding: unicode' do
   <<~RUBY
   
-    host: db
-    user: postgres
-    password: postgres
+      host: db
+      user: postgres
+      password: postgres
   RUBY
 end
